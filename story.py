67173562f1cd4b18e8da6ab2b94382f58473af3d
Amari-dot <119463714+Amari-dot@users.noreply.github.com>
2024-04-24 20:32:37 +0000

    story.py

---
+++ story.py
@@ -0,0 +1,3 @@
+def intro_scene():
+  print("""The current time is 3:00 AM. You are starving. Hungry enough to eat a horse, or even a lawnmower. Specifically a lawnmower from Home Depot. \n You jump out of bed, eager for a feast. You quickly approach Home Depot, as running at hyper-sonic speeds is something natural for a Home Depot veteran. \n \"Hey Kid.\" \n Says someone behind the door, but it isn't Dream. Who could it be? You walk forward, ever so slightly, and ELMO comes dashing towards you. \n FIGHT! Which Weapon will you pick up? \n Sword \n Bow \n Wand.""")
+  class_choice = input("Your choice: ")
