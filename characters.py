5e6d30f37b548422b4be79a8b0d2d03cc6ec7606
ItsZJ <111366126+ItsZJ@users.noreply.github.com>
2024-04-24 20:41:59 +0000

    characters.py

---
+++ characters.py
@@ -0,0 +1,12 @@
+class Character:
+  def __init__(self, name, health, damage):
+    self.name = name
+    self.health = health
+    self.damage = damage
+
+  def attack(self, other):
+    other.health -= self.damage
+    print(f"{self.name} attacks {other.name} for {self.damage} damage!")
+
+  def is_alive(self):
+    return self.health > 0
