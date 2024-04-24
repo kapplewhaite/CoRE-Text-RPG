8b966c180c7fce8ffab14a6d9b17f6e59851670a
Kaiden A. <69645495+kapplewhaite@users.noreply.github.com>
2024-04-24 20:37:09 +0000

    Fix it, please

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
+from story import intro_scene
+
 print("H e l l o W o r l d")
+intro_scene()
--- story.py
+++ story.py
@@ -1,3 +1,11 @@
 def intro_scene():
-  print("""The current time is 3:00 AM. You are starving. Hungry enough to eat a horse, or even a lawnmower. Specifically a lawnmower from Home Depot. \n You jump out of bed, eager for a feast. You quickly approach Home Depot, as running at hyper-sonic speeds is something natural for a Home Depot veteran. \n \"Hey Kid.\" \n Says someone behind the door, but it isn't Dream. Who could it be? You walk forward, ever so slightly, and ELMO comes dashing towards you. \n FIGHT! Which Weapon will you pick up? \n Sword \n Bow \n Wand.""")
+  print("""The current time is 3:00 AM. You are starving. Hungry enough to eat a horse, or even a lawnmower. Specifically a lawnmower from Home Depot.
+You jump out of bed, eager for a feast. You quickly approach Home Depot, as running at hyper-sonic speeds is a natural phenomenon for a Home Depot veteran. 
+"Hey Kid" 
+Says someone behind the door, but it isn't Dream. 
+Who could it be? You walk forward, ever so slightly, and ELMO comes dashing towards you. It seems as though he has taken more than his daily dosage of crack, and is about to hit you. 
+Which Weapon will you pick up? 
+Sword 
+Bow 
+Wand.""")
   class_choice = input("Your choice: ")
